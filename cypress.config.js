c6e2d305cdac0c4f97ac838be210bdf61107b471
kapsalonadem <kapsalonadem@gmail.com>
2025-05-16 04:29:16 +0200

    project ID changed

--- cypress.config.js
+++ cypress.config.js
@@ -3,14 +3,12 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: 'https://gorest.co.in/public/v2',
-    specPattern: 'cypress/integration/**/*.js', // Ensure this matches your file path,
-    projectId: "1cgwu4",
-    
-      "env": {
-        "CYPRESS_API_TOKEN": process.env.CYPRESS_API_TOKEN
-      }
+    baseUrl: "https://gorest.co.in/public/v2",
+    specPattern: "cypress/integration/**/*.js", // Ensure this matches your file path,
+    projectId: "ja5f7v",
+
+    env: {
+      CYPRESS_API_TOKEN: process.env.CYPRESS_API_TOKEN,
+    },
   },
-    
-  
 });
